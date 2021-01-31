15bf7f5b935f2131aab991be766871d93446e2df
Leveditor <leveditorfc@gmail.com>
2021-01-31 11:43:02 -0300

    Adicionando um Footer a página

---
+++ src/component/footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './style.css'
+
+class Footer extends Component {
+    render() {
+        return (
+              <div>
+                  {/* FOOTER */}
+                  <footer className="footer">
+                      <span>Todos os direitos reservados 2021</span>
+                  </footer>
+              </div>
+        );
+    }
+}
+
+export default Footer;
